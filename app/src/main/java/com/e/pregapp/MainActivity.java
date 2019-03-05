8cc9fbcefe4b9269a953bf7f7aeee9a79ca83546
Adebuga <47428632+Adebuga@users.noreply.github.com>
2019-03-05 09:32:14 +0300

    clair

---
+++ app/src/main/java/com/e/pregapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.e.pregapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    NAME:
+//    REGNO:
+//    COURSE:
+//    LECTURER:
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+public void letGo(View view){
+    Intent intent= new Intent(this,Main2Activity.class);
+    startActivity(intent);
+
+}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"starting",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
